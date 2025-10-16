90baaa4c4dcaa145efa4cf99145f5c20119240aa
takuya beppu <bptaku@takuyas-Mac-mini.local>
2025-10-16 17:23:50 +0900

    fix: add default export to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { useMemo, useState } from "react";
 
 // test redeploy
 
-
+export default function Page() {
 /* =========================================================
    定義
    ========================================================= */
